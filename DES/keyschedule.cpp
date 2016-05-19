2f7ef6fcf0c357ad328eda4b1faf7931015be2bc
eugenol <eug@live.co.za>
2016-05-19 20:35:20 +0200

    moved text key creation to keyschedule.cpp

--- DES/keyschedule.cpp
+++ DES/keyschedule.cpp
@@ -2,28 +2,69 @@
 #include <algorithm>
 #include <iterator>
 
+//Constructor with bitset
 keyschedule::keyschedule(std::bitset<64> key)
 {
 	do_keyschedule(key);
 }
+//Constructor with string
+keyschedule::keyschedule(std::string key)
+{
+	int keylen = key.length();
+	int padlength;
+	char tempKey[8];
+	unsigned char* keyPtr;
 
+	//pad the key if it is less than 8 characters, else just truncate it
+	if (keylen < 8)
+	{
+		padlength = 8 - keylen % 8; //padding length
+
+		int keylength_new = keylen + padlength; 
+
+		for (int i = keylen; i < keylength_new; ++i)
+		{
+			key.insert(key.end(), (char)padlength); //pad
+		}
+	}
+	//copy to a character array, only the first 8 characters
+	for (int i = 0; i < 8; ++i)
+	{
+		strncpy(tempKey, key.c_str(), 8);
+	}
+	// to avoid signed/unsigned conversion problems, cast to unsigned char
+	keyPtr = (unsigned char*)tempKey;
+
+	std::bitset<64> bitkey;
+
+	for (int j = 0; j < 8;++j)
+	{
+		std::bitset<64> temp = keyPtr[j];
+		bitkey <<= 8;
+		bitkey |= temp;
+	}
+
+	do_keyschedule(bitkey);
+}
+
+//Default Constructor
 keyschedule::keyschedule( )
 {
 
 }
-
+//Destructor
 keyschedule::~keyschedule()
 {
 }
-
+//Key Schedule algorithm
 void keyschedule::do_keyschedule(std::bitset<64> key)
 {
-	std::bitset<56> keyplus;
-	std::bitset<28> C0;
-	std::bitset<28> D0;
-	std::bitset<28> Cn[16];
-	std::bitset<28> Dn[16];
-	std::bitset<56> CnDn[16];
+	std::bitset<56> keyplus;	//After PC-1
+	std::bitset<28> C0;			//Left half
+	std::bitset<28> D0;			//Right half
+	std::bitset<28> Cn[16];		// all 16 left halves
+	std::bitset<28> Dn[16];		// all 16 right halves
+	std::bitset<56> CnDn[16];	// all 16 keys before PC-2
 
 	// Apply PC-1 to key
 	for (int i = 0; i<56; i++)
@@ -38,10 +79,10 @@ void keyschedule::do_keyschedule(std::bitset<64> key)
 		D0[i] = keyplus[i];
 	}
 
-	// rotate left and right keys
+
 	Cn[0] = C0;
 	Dn[0] = D0;
-
+	// rotate left and right keys
 	for (int i = 0; i < 16; ++i)
 	{
 		rotate_left(Cn[i], shifts[i]);
@@ -73,17 +114,17 @@ void keyschedule::do_keyschedule(std::bitset<64> key)
 		}
 	}
 }
-
+// rotate keys left by m
 void keyschedule::rotate_left(std::bitset<28> &b, int m)
 {
 	b = b << m | b >> (28 - m);
 }
-
+//overloaded operator
 std::bitset<48> keyschedule::operator[](std::size_t idx)
 {
 	return keys[idx];
 };
-
+//reverse for decryption
 void keyschedule::reverse()
 {
 	std::reverse(std::begin(keys), std::end(keys));
--- DES/keyschedule.h
+++ DES/keyschedule.h
@@ -1,12 +1,24 @@
+/*
+This is the keyschedule for the DES Algorithm
+It takes as an argument a 64bit key as a bitset
+On creation of the object it will create the 16
+subkeys needed for DES encryption.
+To access a subkey, use the [] operator on the
+object.
+Bitsets are numbered from LSB to MSB, so LSB is
+bitset[0], opposite from how arrays work.
+*/
+
 #pragma once
 #include <bitset>
+#include <string>
 
 class keyschedule
 {
 private:
-	std::bitset<48> keys[16];
+	std::bitset<48> keys[16]; //stores the 16 subkeys of 48bits each
 
-
+	// PC-1 Table
 	int PC_1[56] = {57,	49,	41,	33,	25,	17,	9,
 					1,	58,	50,	42,	34,	26,	18,
 					10,	2,	59,	51,	43,	35,	27,
@@ -15,9 +27,11 @@ private:
 					7,	62,	54,	46,	38,	30,	22,
 					14,	6,	61,	53,	45,	37,	29,
 					21,	13,	5,	28,	20,	12,	4};
-
+	
+	//Number of shifs for each round
 	int shifts[16] = {1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1};
 
+	// PC-2 Table
 	int PC_2[48] = {14,	17,	11,	24,	1,	5,
 					3,	28,	15,	6,	21,	10,
 					23,	19,	12,	4,	26,	8,
@@ -28,14 +42,15 @@ private:
 					46,	42,	50,	36,	29,	32};
 
 
-	void rotate_left(std::bitset<28> &key, int shift);
-	void do_keyschedule(std::bitset<64> key);
+	void rotate_left(std::bitset<28> &key, int shift); //rotate the bits left by shift amount
+	void do_keyschedule(std::bitset<64> key); //actual function that does the keyschdule
 
 public:
-	keyschedule(std::bitset<64> key);
-	keyschedule();
-	~keyschedule();
-	std::bitset<48> keyschedule::operator[](std::size_t idx);
-	void reverse();
+	keyschedule(std::bitset<64> key); //constructor with bitset
+	keyschedule::keyschedule(std::string key); //constructor with string
+	keyschedule(); //Default constructor, allows for copying object
+	~keyschedule(); //Destructor
+	std::bitset<48> keyschedule::operator[](std::size_t idx); //Access subkey by overloading
+	void reverse(); //reverse keys for decryption
 };
 
